f3219ff575fcb68517466a6736187b914a050a45
EzequielDiez <ezequieldiez.emd@gmail.com>
2025-01-15 01:16:03 -0300

    update

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import onlineIdImage from "../assets/images/online-id.png";
-import feliImage from "../assets/images/feli.png"
-import nanoImage from "../assets/images/nano.png";
+import feliImage from "../assets/images/feli2.png";
+import nanoImage from "../assets/images/nano2.png";
+import nachoImage from "../assets/images/nacho2.png";
+import gastiImage from "../assets/images/gasti2.png";
+import luluImage from "../assets/images/lulu2.png";
+import agusImage from "../assets/images/agustin2.png";
+import ayluImage from "../assets/images/aylu2.png";
+
 
 function SobreNosotros() {
     return (
@@ -27,14 +33,49 @@ function SobreNosotros() {
                     <p className="absolute bottom-64 left-1/3 text-5xl font-acumin">FELICITAS</p>
                     <p className="absolute bottom-64 left-1/2 text-5xl font-sweetie">(FELI, FE, FELA)</p>
                     <br />
-                    <p className="absolute bottom-48 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                    <p className="absolute bottom-44 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
                 </div>
-                <div className="relative -mt-96">
-                    <img src={nanoImage} alt="Nano Imagen" className="w-[1285px]" />
-                    <p className="absolute bottom-64 left-1/3 text-5xl font-acumin">EZEQUIEL</p>
-                    <p className="absolute bottom-64 left-1/2 text-5xl font-sweetie">(NANO, EZE, NONONANO)</p>
+                <div className="relative -mt-36">
+                    <img src={nanoImage} alt="Nano Imagen" className="w-[1246px] -ml-[100px]" />
+                    <p className="absolute bottom-52 left-1/3 text-5xl font-acumin">EZEQUIEL</p>
+                    <p className="absolute bottom-52 left-1/2 text-5xl font-sweetie">(NANO, EZE, NONONANO)</p>
                     <br />
-                    <p className="absolute bottom-48 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                    <p className="absolute bottom-32 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                </div>
+                <div className="relative -mt-28">
+                    <img src={nachoImage} alt="Nacho Imagen" className="w-[1116px]" />
+                    <p className="absolute bottom-56 left-1/3 text-5xl font-acumin">IGNACIO</p>
+                    <p className="absolute bottom-56 left-1/2 text-5xl font-sweetie">(NACHO, NACHI, CHONA)</p>
+                    <br />
+                    <p className="absolute bottom-36 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                </div>
+                <div className="relative -mt-36">
+                    <img src={gastiImage} alt="Gasti Imagen" className="w-[1142px] -ml-[58px]" />
+                    <p className="absolute bottom-48 left-1/3 text-5xl font-acumin">GASTÓN</p>
+                    <p className="absolute bottom-48 left-1/2 text-4xl font-sweetie">(GASI, GASTY, TIGAS, GASTIN, GAS, TONGA...)</p>
+                    <br />
+                    <p className="absolute bottom-28 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                </div>
+                <div className="relative -mt-24">
+                    <img src={luluImage} alt="Lulu Imagen" className="w-[1185px] -ml-[52px]" />
+                    <p className="absolute bottom-64 left-1/3 text-5xl font-acumin">LOURDES</p>
+                    <p className="absolute bottom-64 left-1/2 text-5gixl font-sweetie">(LOLO, LULU, LULO, LU, NENITA)</p>
+                    <br />
+                    <p className="absolute bottom-44 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                </div>
+                <div className="relative -mt-36">
+                    <img src={agusImage} alt="Agus Imagen" className="w-[1116px]" />
+                    <p className="absolute bottom-56 left-1/3 text-5xl font-acumin">AGUSTÍN</p>
+                    <p className="absolute bottom-56 left-1/2 text-5xl font-sweetie">(AGUS, AUSTIN, MARTITEGUI)</p>
+                    <br />
+                    <p className="absolute bottom-36 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
+                </div>
+                <div className="relative -mt-32">
+                    <img src={ayluImage} alt="Aylu Imagen" className="w-[1142px] -ml-[58px]" />
+                    <p className="absolute bottom-48 left-1/3 text-5xl font-acumin">AYLEN</p>
+                    <p className="absolute bottom-48 left-1/2 text-5xl font-sweetie">(AYLU, AY, LU, POLVITOS)</p>
+                    <br />
+                    <p className="absolute bottom-28 left-1/3 text-xl font-acumin max-w-2xl">Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.</p>
                 </div>
             </div>
         </>
