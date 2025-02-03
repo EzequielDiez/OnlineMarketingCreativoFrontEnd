cfdb21fac6b7d612a60949bcaf6312ff4dc4070b
EzequielDiez <ezequieldiez.emd@gmail.com>
2025-02-03 13:53:01 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Inicio from './pages/Inicio';
 import SobreNosotros from './pages/SobreNosotros';
 import Servicios from './pages/Servicios';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/sobre-nosotros" element={<SobreNosotros />} />
         <Route path="/servicios" element={<Servicios />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
       <Footer />
     </div>
