322a077abe52af9638779aeef7e2a1cb043a56ba
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2025-02-21 17:52:08 -0300

    update AboutUs

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -26,96 +26,96 @@ function SobreNosotros() {
                     </div>
                 </div>
             </div>
-            <div className="w-full bg-[#F0E6DC]">
+            <div className="w-full bg-[#F0E6DC] pb-20 sm:pb-32">
                 <div className="relative">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                    <div className="absolute top-[50%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">FELICITAS</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(FELI, FE, FELA)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">FELICITAS</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(FELI, FE, FELA)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={feliImage} alt="Feli Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={feliImage} alt="Feli Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-8 xs:-mt-4 sm:-mt-16 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32">
+                    <div className="absolute top-[55%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">EZEQUIEL</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(NANO, EZE, NONONANO)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">EZEQUIEL</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(NANO, EZE, NONONANO)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={nanoImage} alt="Nano Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={nanoImage} alt="Nano Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-20 xs:mt-2 sm:-mt-8 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32">
+                    <div className="absolute top-[50%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">IGNACIO</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(NACHO, NACHI, CHONA)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">IGNACIO</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(NACHO, NACHI, CHONA)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={nachoImage} alt="Nacho Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={nachoImage} alt="Nacho Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-16 xs:-mt-6 sm:-mt-12 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32">
+                    <div className="absolute top-[50%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">GASTÓN</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(GASI, GASTY, TIGAS, GASTIN, GAS, TONGA...)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">GASTÓN</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(GASI, GASTY, TIGAS, GASTIN, GAS, TONGA...)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={gastiImage} alt="Gasti Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={gastiImage} alt="Gasti Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-20 xs:mt-6 sm:-mt-2 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32">
+                    <div className="absolute top-[50%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">LOURDES</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(LOLO, LULU, LULO, LU, NENITA)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">LOURDES</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(LOLO, LULU, LULO, LU, NENITA)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={luluImage} alt="Lulu Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={luluImage} alt="Lulu Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-16 xs:mt-6 sm:-mt-12 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32 mb-8 sm:mb-0">
+                    <div className="absolute top-[50%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">AGUSTÍN</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(AGUS, AUSTIN, MARTITEGUI)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">AGUSTÍN</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(AGUS, AUSTIN, MARTITEGUI)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={agusImage} alt="Agus Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={agusImage} alt="Agus Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
 
-                <div className="relative -mt-32 sm:-mt-12 md:-mt-16 lg:-mt-20 xl:-mt-24 2xl:-mt-32">
-                    <div className="absolute top-[50%] lg:ml-[20%] z-10">
+                <div className="relative mt-16 xs:-mt-4 sm:-mt-12 md:-mt-6 lg:-mt-10 xl:-mt-24 2xl:-mt-32 mb-8 sm:mb-0">
+                    <div className="absolute top-[55%] ml-[30%] lg:ml-[20%] z-10">
                         <div className="flex items-center gap-2 sm:gap-3 md:gap-4">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-acumin">AYLEN</p>
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl font-sweetie">(AYLU, AY, LU, POLVITOS)</p>
+                            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-acumin">AYLEN</p>
+                            <p className="text-lg sm:text-2xl md:text-3xl lg:text-3xl 2xl:text-5xl font-sweetie">(AYLU, AY, LU, POLVITOS)</p>
                         </div>
-                        <p className="text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
+                        <p className="text-sm md:text-lg lg:text-xl 2xl:text-xl font-acumin mt-2 sm:mt-3 md:mt-4">
                             Somos un conjunto de profesionales en diversas áreas, desde diseño gráfico, programación, community management, y social.
                         </p>
                     </div>
-                    <img src={ayluImage} alt="Aylu Imagen" className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] 2xl:w-[60%]" />
+                    <img src={ayluImage} alt="Aylu Imagen" className="w-[95%] md:w-[90%] lg:w-[60%] 2xl:w-[60%]" />
                 </div>
             </div>
         </>
