8f758141eb5ca58953be80430f75987255dcd3d0
EzequielDiez <ezequieldiez.emd@gmail.com>
2025-02-22 18:06:52 -0300

    Build para GitHub Pages en docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/OnlineMarketingCreativoFrontEnd/', // Reemplaza con el nombre de tu repositorio
+  build: {
+    outDir: 'docs'  // Cambiamos dist por docs
+  }
 })
